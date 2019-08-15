63d4d9de6ab7521a13e299a043bcfd35fce26f8a
hetiantian <969795191@qq.com>
2019-08-15 12:35:07 +0800

    add src

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/Runner.java
@@ -0,0 +1,14 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ * */
+@SpringBootApplication
+public class Runner {
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+}
