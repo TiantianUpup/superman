7d21b11871e0e30daba8d87b1ffd45faef963de7
hetiantian <969795191@qq.com>
2019-11-07 15:02:50 +0800

    add swagger2

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/configuration/Swagger2Configuration.java
@@ -0,0 +1,43 @@
+package ${package}.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger2配置文件
+ *
+ * @author hetiantian
+ * @version 1.0
+ * @Date 2019/11/07 14:19
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //TODO adjust package name
+                .apis(RequestHandlerSelectors.basePackage("your package.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("title name")
+                .version("1.0")
+                //描述
+                .description("API description")
+                .build();
+    }
+}
