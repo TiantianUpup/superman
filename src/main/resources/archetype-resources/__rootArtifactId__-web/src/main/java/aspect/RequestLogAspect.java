b43819683105ac76836c092edc55dfc01c29b524
hetiantian <969795191@qq.com>
2020-03-12 15:00:54 +0800

    add error request log aspect

--- src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/aspect/RequestLogAspect.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/aspect/RequestLogAspect.java
@@ -1,15 +1,21 @@
 package ${package}.aspect;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * RequestLogAspect
@@ -27,33 +33,40 @@ public class RequestLogAspect {
     public void requestServer() {
     }
 
-    @Before("requestServer()")
-    public void doBefore(JoinPoint joinPoint) {
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-
-        LOGGER.info("===============================Start========================");
-        LOGGER.info("IP                 : {}", request.getRemoteAddr());
-        LOGGER.info("URL                : {}", request.getRequestURL().toString());
-        LOGGER.info("HTTP Method        : {}", request.getMethod());
-        LOGGER.info("Class Method       : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-    }
-
-
     @Around("requestServer()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         long start = System.currentTimeMillis();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
         Object result = proceedingJoinPoint.proceed();
-        LOGGER.info("Request Params     : {}", getRequestParams(proceedingJoinPoint));
-        LOGGER.info("Result               : {}", result);
-        LOGGER.info("Time Cost            : {} ms", System.currentTimeMillis() - start);
+        RequestInfo requestInfo = new RequestInfo();
+        requestInfo.setIp(request.getRemoteAddr());
+        requestInfo.setUrl(request.getRequestURL().toString());
+        requestInfo.setHttpMethod(request.getMethod());
+        requestInfo.setClassMethod(String.format("%s.%s", proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName()));
+        requestInfo.setRequestParams(getRequestParamsByProceedingJoinPoint(proceedingJoinPoint));
+        requestInfo.setResult(result);
+        requestInfo.setTimeCost(System.currentTimeMillis() - start);
+        LOGGER.info("Request Info      : {}", JSON.toJSONString(requestInfo));
 
         return result;
     }
 
-    @After("requestServer()")
-    public void doAfter(JoinPoint joinPoint) {
-        LOGGER.info("===============================End========================");
+
+    @AfterThrowing(pointcut = "requestServer()", throwing = "e")
+    public void doAfterThrow(JoinPoint joinPoint, RuntimeException e) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        RequestErrorInfo requestErrorInfo = new RequestErrorInfo();
+        requestErrorInfo.setIp(request.getRemoteAddr());
+        requestErrorInfo.setUrl(request.getRequestURL().toString());
+        requestErrorInfo.setHttpMethod(request.getMethod());
+        requestErrorInfo.setClassMethod(String.format("%s.%s", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName()));
+        requestErrorInfo.setRequestParams(getRequestParamsByJoinPoint(joinPoint));
+        requestErrorInfo.setException(e);
+        LOGGER.info("Error Request Info      : {}", JSON.toJSONString(requestErrorInfo));
     }
 
     /**
@@ -62,14 +75,26 @@ public class RequestLogAspect {
      *
      * @return
      * */
-    private Map<String, Object> getRequestParams(ProceedingJoinPoint proceedingJoinPoint) {
-        Map<String, Object> requestParams = new HashMap<>();
-
+    private Map<String, Object> getRequestParamsByProceedingJoinPoint(ProceedingJoinPoint proceedingJoinPoint) {
         //参数名
         String[] paramNames = ((MethodSignature)proceedingJoinPoint.getSignature()).getParameterNames();
         //参数值
         Object[] paramValues = proceedingJoinPoint.getArgs();
 
+        return buildRequestParam(paramNames, paramValues);
+    }
+
+    private Map<String, Object> getRequestParamsByJoinPoint(JoinPoint joinPoint) {
+        //参数名
+        String[] paramNames = ((MethodSignature)joinPoint.getSignature()).getParameterNames();
+        //参数值
+        Object[] paramValues = joinPoint.getArgs();
+
+        return buildRequestParam(paramNames, paramValues);
+    }
+
+    private Map<String, Object> buildRequestParam(String[] paramNames, Object[] paramValues) {
+        Map<String, Object> requestParams = new HashMap<>();
         for (int i = 0; i < paramNames.length; i++) {
             Object value = paramValues[i];
 
@@ -83,5 +108,26 @@ public class RequestLogAspect {
         }
 
         return requestParams;
+    }
+
+    @Data
+    public class RequestInfo {
+        private String ip;
+        private String url;
+        private String httpMethod;
+        private String classMethod;
+        private Object requestParams;
+        private Object result;
+        private Long timeCost;
+    }
+
+    @Data
+    public class RequestErrorInfo {
+        private String ip;
+        private String url;
+        private String httpMethod;
+        private String classMethod;
+        private Object requestParams;
+        private RuntimeException exception;
     }
 }
